86a6baacc0aebcbeaa8be5f417a85008b598b4b4
hexpool <30502705+hexpool@users.noreply.github.com>
2017-08-03 09:47:38 +0200

    Delete 2017-08-03-notifications.sql

--- sql/2017-08-03-notifications.sql
+++
@@ -1,3 +0,0 @@
--- correct notifications
-ALTER TABLE `notifications` CHANGE `lasttriggered` `lasttriggered` INT(10) UNSIGNED NULL;
-ALTER TABLE `notifications` CHANGE `lastchecked` `lasttriggered` INT(10) UNSIGNED NULL;
